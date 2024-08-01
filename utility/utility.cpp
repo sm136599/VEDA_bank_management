4bd7558351b83bb59e788e805dd5f07dcc2e7357
박성민 <sm136599@gmail.com>
2024-08-01 15:11:24 +0900

    feat: 날짜 출력 유틸 추가

--- utility/utility.cpp
+++ utility/utility.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
@@ -15,5 +16,19 @@ string num_to_string_with_comma(int num) {
         cnt++;
         num /= 10;
     }
+    return ret;
+}
+
+string make_time_string(time_t now) {
+    tm* p_time_info;
+    p_time_info = localtime(&now);
+    int year = p_time_info->tm_year + 1900;
+    int month = p_time_info->tm_mon + 1;
+    int day = p_time_info->tm_mday;
+    int hour = p_time_info->tm_hour;
+    int min = p_time_info->tm_min;
+    int sec = p_time_info->tm_sec;
+    string ret = "";
+    ret += to_string(year) + "년 " + to_string(month) + "월 " +to_string(day) + "일";
     return ret;
 }
