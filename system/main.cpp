1de490516eb094c57e5cf555b502cc046a885124
Seohyun <63396820+jejuvita01@users.noreply.github.com>
2024-08-01 15:10:45 +0900

    modified main.cpp

--- system/main.cpp
+++ system/main.cpp
@@ -5,9 +5,11 @@
 
 using namespace std;
 
+#include "utility.h"
 #include "person.h"
 #include "user.h"
 #include "manager.h"
+
 
 map<string, pair<string, Person*>> sys; // id: <pw,person>
 string id, pw;
@@ -21,9 +23,59 @@ void init_data()
     cout << "\n\n";
 }
 
+void search_user()
+{
+    id = "";
+    while (id != "exit") {
+        cout << "\n\n조회할 유저의 id를 입력하세요(종료를 원할시 \"exit\" 입력): \n";
+        cin >> id;
+        if (id == "exit")
+            break;
+        if (sys.find(id) == sys.end())
+            cout << "해당 id를 가진 유저가 없습니다.\n";
+        else {
+            cout << "*** " << id << " 유저의 정보 ***\n";
+            cout << "유저의 이름: " << sys[id].second->get_name() << '\n';
+            cout << "유저의 나이: " << sys[id].second->get_age() << '\n';
+//            cout << "유저의 계좌: 총 " <<
+        }
+    }
+    cout << "\n... 조회를 종료합니다 ...\n\n";
+}
+
+void manager_screen(void)
+{
+    int cmd = 0;
+    
+    while (cmd != 1 && cmd != 9) {
+//        system("cls");
+        cout << "\n\n... 매니저 모드에 진입했습니다 ...\n";
+        
+        // 1: 유저 조회 ... search_user()
+        cout << "1: 유저 조회\n";
+        
+        // 2: 유저 삭제 ... delete_user()
+        cout << "2: 유저 삭제\n";
+        
+        // 9: 매니저 모드 종료 ... init_screen()
+        cout << "9: 매니저 모드 종료\n";
+        
+        cout << "메뉴를 선택하세요: ";
+        cin >> cmd;
+        
+//        switch (cmd) {
+//            case 1: search_user(); return; break;
+//            case 2: delete_user(); break;
+//            case 9: init_screen(); return;  break;
+//            default: cout << "다시 입력하세요.\n"; init_screen(); break;
+//        }
+    }
+}
+
+// 유저 조회(내부에서 계좌 조회)/ 유저 삭제
 void manager_mode()
 {
-    cout << "\n\n... 매니저 모드에 진입했습니다 ...\n\n";
+    manager_screen();
 }
 
 void user_mode()
@@ -61,9 +113,8 @@ void login()
 void description(void)
 {
     cout << "\n\nVEDA 은행에는 다음과 같은 상품이 있습니다.\n";
-    cout << "1. 예금: 2,147,483,648 원 한도, 연 3%(단리)\n";
-    cout << "2. 희망적금: 1년간 월 최대 100만원 입금, 연 5%(단리)\n";
-    cout << "3. 도약적금: 5년간 월 최대 100만원 입금, 연 7%(단리)\n";
+    cout << "1. 보통예금: 10,000만원 한도, 연 3%(단리)\n";
+    cout << "2. 정기예금: 1년 만기, 최대 5,000만원 입금, 연 5%(단리)\n";
     cout << "회원가입시 (1. 예금) 상품은 자동 개설됩니다.\n";
     cout << "많은 이용 바랍니다.\n\n\n";
 }
